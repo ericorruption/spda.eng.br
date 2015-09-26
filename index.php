07fb1dab8c4372afef71aab759a2f26749df0217
Eric Quanz <eric.quanz@gmail.com>
2015-09-25 21:21:24 -0300

    current version of project

---
+++ index.php
@@ -0,0 +1,432 @@
+<?php
+if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['telefone'])
+    && isset($_POST['mensagem'])){
+
+    //REMETENTE --> ESTE EMAIL TEM QUE SER VALIDO DO DOMINIO
+    //====================================================
+    $email_remetente = "spda@spda.eng.br"; // deve ser um email do dominio
+    //====================================================
+
+    //Configurações do email, ajustar conforme necessidade
+    //====================================================
+    $email_destinatario = "spda@spda.eng.br"; // qualquer email pode receber os dados
+    $email_reply = "$email";
+    $email_assunto = "SPDA Engenharia+Projetos - Contato via site";
+    //====================================================
+
+
+    //Variaveis de POST, Alterar somente se necessário
+    //====================================================
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $mensagem = $_POST['mensagem'];
+    //====================================================
+
+    //Monta o Corpo da Mensagem
+    //====================================================
+    $email_conteudo = "Nome = $nome \n";
+    $email_conteudo .= "Email = $email \n";
+    $email_conteudo .=  "Telefone = $telefone \n";
+    $email_conteudo .=  "Mensagem = $mensagem \n";
+    //====================================================
+
+    //Seta os Headers (Alerar somente caso necessario)
+    //====================================================
+    $email_headers = implode ( "\n",array ( "From: $email_remetente", "Reply-To: $email_reply", "Subject: $email_assunto","Return-Path:  $email_remetente","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ) );
+    //====================================================
+
+
+    //Enviando o email
+    //====================================================
+    if (mail ($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_headers)){
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'true'){
+            echo "1";
+            die();
+        } else {
+            $isSuccess = true;
+        }
+    } else {
+        $error = true;
+    }
+
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>SPDA Engenharia+Projetos</title>
+    <meta name="description" content="Engenharia elétrica clara e de qualidade. Situada em Porto Alegre/RS" />
+
+    <!-- Default favicons. You should have at least two 16px icons: -->
+    <!-- one .png and one .ico file in your root for this to work. -->
+    <link rel="icon" href="favicon.png">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Review the viewport rule in the SASS file if you change the defaults below -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <script>window.html5 || document.write('<script src="js/libs/html5.min.js"><\/script>')</script>
+    <script src="js/libs/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<header class="main-header" role="banner" id="top">
+    <div class="main-header-content">
+        <img class="logo" src="img/logo.png" alt="Assinatura visual SPDA">
+        <h1 class="main-title">SPDA Engenharia+Projetos</h1>
+
+        <a href="#main-content" class="goto-content">Ir para o conteúdo principal</a>
+
+        <nav class="nav main-nav" role="navigation" id="nav">
+            <a href="#o-que-fazemos" class="std-btn main-nav-item">O que fazemos?</a>
+            <a href="#quem-somos" class="std-btn main-nav-item">Quem somos?</a>
+            <a href="#clientes" class="std-btn main-nav-item">Clientes</a>
+            <a href="#contato" class="std-btn main-nav-item">Contato</a>
+        </nav>
+
+        <p class="main-tagline">Somos a SPDA.<br/> Produzimos engenharia elétrica de qualidade.</p>
+    </div>
+</header>
+
+<main class="main-content" id="main-content" role="main">
+    <article class="std-article services-article">
+            <h2 class="article-title" id="o-que-fazemos"><span>O que fazemos?</span></h2>
+
+            <section class="service-item">
+                <div class="service-image-wrapper">
+                    <div class="service-image-background">
+                        <img src="img/o-que-fazemos/instalacoes-eletricas.png" class="service-image" />
+                    </div>
+                </div>
+
+                <h3 class="service-title">Instalações Elétricas</h3>
+                <p>Projetos em Baixa e Média Tensão</p>
+            </section>
+
+            <section class="service-item">
+                <div class="service-image-wrapper">
+                    <div class="service-image-background">
+                        <img src="img/o-que-fazemos/cabeamento-estruturado.png" class="service-image" />
+                    </div>
+                </div>
+
+                <h3 class="service-title">Cabeamento Estruturado</h3>
+                <p>Projetos de Redes de Lógica e Telecomunicações</p>
+            </section>
+
+            <section class="service-item">
+                <div class="service-image-wrapper">
+                    <div class="service-image-background">
+                        <img src="img/o-que-fazemos/spda.png" class="service-image" />
+                    </div>
+                </div>
+
+                <h3 class="service-title">SPDA (é claro!)</h3>
+                <p>Projetos de Sistemas de Proteção contra Descargas Atmosféricas, Aterramentos e Equipotencialização</p>
+            </section>
+
+            <section class="service-item">
+                <div class="service-image-wrapper">
+                    <div class="service-image-background">
+                        <img src="img/o-que-fazemos/entrada-de-energia.png" class="service-image" />
+                    </div>
+                </div>
+
+                <h3 class="service-title">Entrada de Energia</h3>
+                <p>Projetos de Subestações, Tarifação, Fator de Potência, Entrada e Medição, Grupos Geradores e UPS</p>
+            </section>
+
+            <section class="service-item">
+                <div class="service-image-wrapper">
+                    <div class="service-image-background">
+                        <img src="img/o-que-fazemos/ppci.png" class="service-image" />
+                    </div>
+                </div>
+
+                <h3 class="service-title">PPCI</h3>
+                <p>Projetos de Prevenção, Detecção e Combate a Incêndios</p>
+            </section>
+
+            <section class="service-item">
+                <div class="service-image-wrapper">
+                    <div class="service-image-background">
+                        <img src="img/o-que-fazemos/seguranca-eletronica.png" class="service-image" />
+                    </div>
+                </div>
+
+                <h3 class="service-title">Segurança Eletrônica</h3>
+                <p>Projetos de CFTV, Alarmes de Intrusão, Controles de Acesso e Automação</p>
+            </section>
+
+            <section class="service-item">
+                <div class="service-image-wrapper">
+                    <div class="service-image-background">
+                        <img src="img/o-que-fazemos/engenharia-consultiva.png" class="service-image" />
+                    </div>
+                </div>
+
+                <h3 class="service-title">Engenharia Consultiva</h3>
+                <p>Avaliações, Vistorias, Fiscalizações de Obras e Consultorias em Eficiência Energética</p>
+            </section>
+
+            <section class="service-item">
+                <div class="service-image-wrapper">
+                    <div class="service-image-background">
+                        <img src="img/o-que-fazemos/projetos-suplementares.png" class="service-image" />
+                    </div>
+                </div>
+
+                <h3 class="service-title">Projetos Suplementares</h3>
+                <p>Projetos de Arquitetura e Climatização</p>
+            </section>
+    </article>
+
+    <article class="std-article about-article">
+        <div class="std-article-content">
+            <h2 class="article-title" id="quem-somos"><span>Quem somos?</span></h2>
+
+            <p>O nome SPDA, que começou de uma forma curiosa, se tornou parte da nossa filosofia.</p>
+
+            <p>Ainda dentro de uma grande empresa, o sócio-fundador da SPDA construiu uma reputação de encarar e resolver qualquer trabalho, até os que chegavam abaixo de mau tempo.</p>
+
+            <p>SPDA significa "Sistema de Proteção contra Descargas Atmosféricas", vulgo "para-raios". Esse nome é uma representação exata para o que buscamos em nosso trabalho.</p>
+
+            <p class="illustration-paragraph">Acreditamos que <em>trabalho cuidadoso</em> durante a fase de projeto <em>reduz a probabilidade de problemas</em> na execução da obra, provém <em>melhor controle de custos</em> e garante <em>qualidade superior</em> no produto final. Além disso, o mesmo trabalho de qualidade nos permite propor soluções técnicas elegantes, seguras e funcionais.</p>
+        </div>
+    </article>
+
+    <article class="std-article clients-article">
+            <h2 class="article-title" id="clientes"><span>Clientes</span></h2>
+
+            <p>Estamos orgulhosos de todos os nossos trabalhos, alguns deles foram feitos para os seguintes clientes:</p>
+
+            <ul class="client-list">
+                <li class="client-list-item">
+                    <div class="client-list-image-wrapper">
+                        <img class="client-list-image" src="img/clientes/banco-central.png" />
+                    </div>
+                    <h3 class="client-list-title">Banco Central do Brasil</h3>
+                </li>
+
+                <li class="client-list-item">
+                    <div class="client-list-image-wrapper">
+                        <img class="client-list-image" src="img/clientes/bb.png" />
+                    </div>
+                    <h3 class="client-list-title">Banco do Brasil</h3>
+                </li>
+
+                <li class="client-list-item">
+                    <div class="client-list-image-wrapper">
+                        <img class="client-list-image" src="img/clientes/banrisul.png" />
+                    </div>
+                    <h3 class="client-list-title">Banrisul</h3>
+                </li>
+
+                <li class="client-list-item">
+                    <div class="client-list-image-wrapper">
+                        <img class="client-list-image" src="img/clientes/caixa.png" />
+                    </div>
+                    <h3 class="client-list-title">Caixa Econômica Federal</h3>
+                </li>
+
+                <li class="client-list-item">
+                    <div class="client-list-image-wrapper">
+                        <img class="client-list-image" src="img/clientes/gvt.png" />
+                    </div>
+                    <h3 class="client-list-title">GVT</h3>
+                </li>
+
+                <li class="client-list-item">
+                    <div class="client-list-image-wrapper">
+                        <img class="client-list-image" src="img/clientes/nacional.png" />
+                    </div>
+                    <h3 class="client-list-title">Nacional</h3>
+                </li>
+
+                <li class="client-list-item">
+                    <div class="client-list-image-wrapper">
+                        <img class="client-list-image" src="img/clientes/taurus.png" />
+                    </div>
+                    <h3 class="client-list-title">Taurus</h3>
+                </li>
+
+                <li class="client-list-item">
+                    <div class="client-list-image-wrapper">
+                        <img class="client-list-image" src="img/clientes/vivo.png" />
+                    </div>
+                    <h3 class="client-list-title">Vivo</h3>
+                </li>
+
+                <li class="client-list-item">
+                    <div class="client-list-image-wrapper">
+                        <img class="client-list-image" src="img/clientes/zaffari.png" />
+                    </div>
+                    <h3 class="client-list-title">Zaffari</h3>
+                </li>
+            </ul>
+    </article>
+
+    <article class="std-article contact-article">
+        <div class="std-article-content">
+            <h2 class="article-title" id="contato"><span>Contato</span></h2>
+
+            <div class="contact-article-content">
+                <p class="contact-info telephone"><span class="contact-info-title">Telefone:</span> <a href="tel:555132351533">+55 (51) 3235.1533</a></p>
+
+                <p class="contact-info address">
+                    <a href="https://www.google.com.br/maps?q=Av.+Get%C3%BAlio+Vargas,+901+-+Cj.+506+Porto+Alegre%2FRS&ie=UTF8&ll=-30.052602,-51.222596&spn=0.012611,0.022724&sll=-30.052782599999627,-51.22168501651431&sspn=0.00723439859431717,0.015450120676564816&t=m&dg=opt&hnear=Av.+Get%C3%BAlio+Vargas,+901+-+Menino+Deus,+Porto+Alegre+-+Rio+Grande+do+Sul,+90150-003&z=16" target="_blank">
+                        <strong>Centro Empresarial Getúlio Vargas</strong><br/>
+                        Av. Getúlio Vargas, 901 - Cj. 506<br/>
+                        CEP 90150-003 - Porto Alegre/RS
+                    </a>
+                </p>
+
+                <p class="contact-info email">
+                    <span class="contact-info-title">Email:</span>
+                    <a href="mailto:spda@spda.eng.br">spda@spda.eng.br</a>
+                </p>
+            </div>
+
+            <?php if ($isSuccess === true) : ?>
+
+            <?php else : ?>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>#contato" role="form" class="std-form contact-form <?php if ($isSuccess === true){ echo 'success'; } ?>" method="post" id="contact-form">
+                <fieldset>
+                    <legend>Formulário de contato </legend>
+
+                    <input name="nome" type="text" class="std-input"
+                    placeholder="Nome" required />
+
+                    <input name="email" type="email" class="std-input"
+                    placeholder="e-mail" required />
+
+                    <input name="telefone" type="text" class="std-input"
+                    placeholder="Telefone" required />
+
+                    <textarea name="mensagem" id="message"
+                    class="std-input std-textarea" placeholder="Mensagem" required></textarea>
+
+                    <button class="std-btn send-form-btn" id="send-form">Enviar</button>
+                    <?php if ($error === true) : ?>
+                        <p class="error-message">Todos os campos são obrigatórios!</p>
+                    <?php endif; ?>
+                </fieldset>
+
+                <p class="success-msg">
+                    <strong>Sua mensagem foi enviada!</strong>
+                    Esqueceu algo? <a href="<?php echo $_SERVER['PHP_SELF']; ?>#contato" id="reset-form">Envie outra mensagem!</a>
+                </p>
+            </form>
+            <?php endif; ?>
+        </div>
+    </article>
+
+    <a href="#top" class="std-btn back-to-top" id="back-to-top" title="Voltar para o topo"><span>Voltar para o topo</span></a>
+</main>
+
+<footer class="main-footer" role="contentinfo">
+    <p class="main-footer-text">&copy; 2013 SPDA Engenharia<br/>
+    CREA-RS 192621</p>
+
+    <p class="main-footer-author">Site:
+        <a href="http://www.leonardohauschild.com" target="_blank" class="author hauschild" title="Leonardo Hauschild">Leonardo Hauschild</a>
+        +
+        <a href="http://ericquanz.com" target="_blank" class="author quanz" title="Eric Quanz">Eric Quanz</a>
+    </p>
+</footer>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.10.2.min.js"><\/script>')</script>
+<script src="js/libs/jquery.smooth-scroll.min.js"></script>
+<script src="js/libs/jquery.validate.min.js"></script>
+<script src="js/libs/jquery.placeholder.min.js"></script>
+<script type="text/javascript">
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+e.src='//www.google-analytics.com/analytics.js';
+r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+ga('create','UA-37069465-1');ga('send','pageview');
+(function($){
+    $.extend($.validator.messages, {
+        required: 'Todos os campos são obrigatórios!',
+        email: 'Preencha com um e-mail válido.'
+    });
+
+    $('#nav a.main-nav-item, #back-to-top').smoothScroll({
+        speed: 1000,
+        offset: -20
+    });
+    $('#contact-form').validate({
+        showErrors: function(){
+            $('#error-msg').remove();
+            this.defaultShowErrors();
+        },
+        errorPlacement: function(error, element) {
+            $('#error-msg').remove();
+            $('<p class="error-msg" id="error-msg">' + error.text() + '</p>').insertAfter('#send-form');
+        },
+        submitHandler: function(form) {
+            var $form = $(form),
+                data = $form.serialize()+'&ajax=true',
+                target = $form.attr('action');
+
+            $.post(target, data, function(data) {
+                if(data == '1') {
+                    $form.addClass('success');
+                    $('.std-input').val('');
+                }
+            });
+        }
+    });
+    $('input.std-input, textarea.std-input').placeholder();
+    $('#reset-form').on('click', function(e){
+        $(this).parent().parent().removeClass('success');
+        e.preventDefault();
+    });
+
+    var $top = $('#top');
+
+    // TODO: Mask / validate telephone ?
+    $(window).load(function(){
+        $(window).on('resize', function() {
+            var windowHeight = $(window).height(),
+                contentHeight = $top.children().height();
+
+            if (windowHeight - 80 <= contentHeight) {
+                $top.children().removeAttr('style');
+            } else {
+                $top.css('min-height', windowHeight)
+                .children().css('padding', Math.ceil((windowHeight-$top.children().height()) / 2) + 'px 0 0 0');
+            }
+        }).resize();
+    })
+
+    var docWidth = $(window).innerWidth();
+    if (docWidth >= 560) {
+        var $backToTop = $('#back-to-top');
+
+        $(window).on('scroll', function() {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > 1220) {
+                $backToTop.addClass('visible');
+
+            } else {
+                var yPos = -(scrollTop / 3),
+                    coords = '50% '+ yPos + 'px';
+
+                $top.css({ backgroundPosition: coords });
+                $backToTop.removeClass('visible');
+            }
+        });
+    }
+}(jQuery));
+</script>
+</body>
+</html>
